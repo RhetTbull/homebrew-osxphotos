6c2b45a18a29e073816188a47988c83f72f863b3
Scott Robertson <git@scott.dev>
2025-06-14 19:13:30 +0100

    Bit of cleanup
    Try this
    
    Try this too
    
    Try updating
    
    Try this now
    
    Debugging
    
    Debugging
    
    Debugging
    
    Try
    
    Debugging
    
    Another thing
    
    urgh
    
    debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Bit of cleanup
    
    Cleanup
    
    Debugging
    
    Debugging

--- Formula/osxphotos.rb
+++ Formula/osxphotos.rb
@@ -3,27 +3,18 @@ class Osxphotos < Formula
 
   desc "Export photos from Apple Photos app and query the Photos database"
   homepage "https://github.com/RhetTbull/osxphotos"
-  url "https://files.pythonhosted.org/packages/source/o/osxphotos/osxphotos-0.72.0.tar.gz"
-  sha256 "6adc48ed4c4e0e055e9a754a3e5fe80c0b5c1b60b8a088b5cba6a725f51a2f65"
+  version "0.72.0"
+  url "https://files.pythonhosted.org/packages/source/o/osxphotos/osxphotos-#{version}.tar.gz"
+  sha256 "26df36a1ae8e1c51046a50f10dddc1484ca91b541945a712b270de357cb13471"
   license "MIT"
 
-  depends_on "python@3.12"
+  depends_on "python@3.10" 
   depends_on :macos
 
   def install
-    virtualenv_install_with_resources
-
-    # Generate shell completions
-    generate_completions_from_executable(bin/"osxphotos", shells: [:bash, :zsh, :fish],
-                                          shell_parameter_format: :click)
-  end
-
-  test do
-    # Test basic functionality
-    assert_match "OSXPhotos", shell_output("#{bin}/osxphotos --version")
-    assert_match "Usage", shell_output("#{bin}/osxphotos --help")
-    
-    # Test that we can import the module
-    system libexec/"bin/python", "-c", "import osxphotos; print('Import successful')"
+    virtualenv_create(libexec, "python3")
+    system libexec/"bin/python", "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"
+    system libexec/"bin/python", "-m", "pip", "install", buildpath.to_s
+    bin.install_symlink libexec/"bin/osxphotos"
   end
 end
