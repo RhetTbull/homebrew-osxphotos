710a365171f839badd67f8a54f15e38129abfd1c
Rhet Turnbull <rturnbull@gmail.com>
2026-09-26 11:03:30 -0500

    Run brew clean (#4)

--- Formula/osxphotos.rb
+++ Formula/osxphotos.rb
@@ -3,13 +3,12 @@ class Osxphotos < Formula
 
   desc "Export photos from Apple Photos app and query the Photos database"
   homepage "https://github.com/RhetTbull/osxphotos"
-  version "0.77.0"
-  url "https://files.pythonhosted.org/packages/source/o/osxphotos/osxphotos-#{version}.tar.gz"
+  url "https://files.pythonhosted.org/packages/70/3b/bfd19eba72f76ab60005adc70c5b4b770e47927a66640a4ce9fbffd67ac8/osxphotos-0.77.0.tar.gz"
   sha256 "fe6b89e100761cef2f5ae2a0d202735f1c0b07218db3265b7dba228cc2619a40"
   license "MIT"
 
-  depends_on "python@3.13"
   depends_on :macos
+  depends_on "python@3.13"
 
   # Rust extensions from PyPI wheels (e.g. whenever, tibs) are dylibs with
   # @rpath install names and too little Mach-O header padding for Homebrew
